9bef03c74d6dd645e0765b05f2d03103c47af98b
Abhi773600 <ajaysharma24373867@gmail.com>
2026-02-17 00:34:31 -0800

    Configured Tailwind and homepage layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,27 @@
-import Counter from "./Counter";
-import { useState } from "react";
 function App() {
-  const [count, setCount] = useState(0);
-
-  const increase = () => {
-    setCount(count + 1);
-  };
-
-  const decrease = () => {
-    setCount(count - 1);
-  };
-
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Skill Tracker Counter 🚀</h1>
-      <h2>{count}</h2>
+    <div className="min-h-screen bg-gray-900 text-white">
+      
+      {/* Navbar */}
+      <nav className="flex justify-between items-center px-8 py-4 bg-gray-800">
+        <h1 className="text-xl font-bold">Skill Tracker</h1>
+        <button className="bg-cyan-500 px-6 py-2 rounded-full font-semibold transition-all duration-500 ease-in-out hover:bg-cyan-600 hover:shadow-[0_0_15px_rgba(34,211,238,0.8)] hover:-translate-y-1">    login /Sign In</button>
+      </nav>
 
-      <Counter title="My First React Props Example 🔥" />
-      <button onClick={increase}>Increase</button>
-      <button onClick={decrease} style={{ marginLeft: "10px" }}>
-        Decrease
-      </button>
+      {/* Hero Section */}
+      <div className="flex flex-col items-center justify-center text-center mt-20 px-4">
+        <h2 className="text-4xl md:text-5xl font-bold mb-6">
+          Track Your Skills. Improve Daily.
+        </h2>
+
+        <p className="text-gray-400 max-w-xl mb-8">
+          Build consistency, track hours, monitor progress and become unstoppable.
+        </p>
+
+        <button className="bg-cyan-500 px-6 py-2 rounded-full font-semibold transition-all duration-500 ease-in-out hover:bg-cyan-600 hover:shadow-[0_0_15px_rgba(34,211,238,0.8)] hover:-translate-y-1">
+          Get Started 🚀
+        </button>
+      </div>
     </div>
   );
 }
