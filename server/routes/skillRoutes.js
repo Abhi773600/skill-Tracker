7446053348de7805da48a27b7be733cbf3237fb8
Abhi773600 <ajaysharma24373867@gmail.com>
2026-02-24 02:42:50 -0800

    Fixed case-sensitive model import issue

--- server/routes/skillRoutes.js
+++ server/routes/skillRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Skill = require('../models/Skill');
+const Skill = require('../models/skill');
 const auth = require('../middleware/authMiddleware');
 
 // ==============================
